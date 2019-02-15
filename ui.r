944e7b234022884a959fdbefebcff21a4c0e6312
ManRod10 <42626680+ManRod10@users.noreply.github.com>
2019-02-15 18:02:41 -0500

    Create ui.r

---
+++ ui.r
@@ -0,0 +1,25 @@
+dashboardPage(
+  dashboardHeader(title = "Text Minining & Noticias"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Descripcion", tabName = "des", icon = icon("dashboard")),
+      menuItem("Visualizacion", tabName = "viz", icon = icon("th"))
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = "des",
+              
+              h4("Esta aplicacion tiene por objetivo monitorear, las noticias economicas de los
+                 principales medios del pais")),
+      
+      tabItem(tabName = "viz",
+              mainPanel(
+                fluidRow(
+                  splitLayout(cellWidths = c("50%", "50%"), 
+                              box(plotOutput("distPlot1"),width = 12),
+                              box(plotOutput("distPlot2"),width = 12))
+                )
+              )
+      )))
+    )
